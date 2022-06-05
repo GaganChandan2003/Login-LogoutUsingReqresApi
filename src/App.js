1f98e68c396a675fa409e0429c35818f5f5bf8f3
GaganChandan2003 <101565932+GaganChandan2003@users.noreply.github.com>
2022-06-05 22:42:17 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Login from './Components/Login';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
